c0c1dd142224a7f6c25e3244d5533aabcebb5179
cmiller01 <christian.david.miller@gmail.com>
2023-07-12 07:33:54 +0000

    sigh: why do this to myself, lint fixup

--- mlb_elo/data.py
+++ mlb_elo/data.py
@@ -1,18 +1,20 @@
-from pybaseball import schedule_and_record, cache # type: ignore
+from pybaseball import schedule_and_record, cache  # type: ignore
 from typing import Tuple
 
 cache.enable()
 
+
 def get_teams() -> Tuple[str, ...]:
     return (
-        "ANA", # Anaheim Angels
-        "ARI", # Diamondbacks
-        "ATL", # Braves
-        "BAL", # Orioles
-        "BOX", # Red Sox
-        "CHC", # Cubs
-        "CHW", # White Sox
+        "ANA",  # Anaheim Angels
+        "ARI",  # Diamondbacks
+        "ATL",  # Braves
+        "BAL",  # Orioles
+        "BOX",  # Red Sox
+        "CHC",  # Cubs
+        "CHW",  # White Sox
     )
+
 
 def main():
     data = schedule_and_record(2023, "OAK")
